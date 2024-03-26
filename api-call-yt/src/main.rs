f40846a5550bdb78c99612c9cdd3327014d0e6dd
shuaiwang <shuaiwang@autox.clu>
2024-03-26 14:32:19 +0800

    finish api-call-yt

---
+++ api-call-yt/src/main.rs
@@ -0,0 +1,52 @@
+use reqwest::header::USER_AGENT;
+use reqwest::Error;
+use serde::Deserialize;
+
+// #[derive(Deserialize, Debug)]
+// struct allData(Vec<[User; 4]>);
+
+#[derive(Deserialize, Debug)]
+struct User {
+    login: String,
+    id: u32,
+    node_id: String,
+    avatar_url: String,
+    gravatar_id: String,
+    url: String,
+    html_url: String,
+    followers_url: String,
+    following_url: String,
+    gists_url: String,
+    starred_url: String,
+    subscriptions_url: String,
+    organizations_url: String,
+    repos_url: String,
+    events_url: String,
+    received_events_url: String,
+    r#type: String,
+    site_admin: bool,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let request_url = format!(
+        "https://api.github.com/repos/{owner}/{repo}/stargazers",
+        owner = "zabbix",
+        repo = "zabbix-docker",
+    );
+    println!("{}", request_url);
+    let client = reqwest::Client::new();
+    let response = client
+        .get(&request_url)
+        .header(
+            USER_AGENT,
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+        )
+        .send()
+        .await?;
+    let users: Vec<User> = response.json::<Vec<User>>().await?;
+
+    println!("{:#?}", users);
+
+    Ok(())
+}
